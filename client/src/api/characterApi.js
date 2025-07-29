05e95e3ff7efc314e98c46e82f465eb7c4948547
djsodope <didieryjb@gmail.com>
2025-07-29 13:59:46 -0400

    new file for backend to talk to front end

---
+++ client/src/api/characterApi.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api/characters';
+
+export const createCharacter = async (character) => {
+  const response = await axios.post(API_URL, character);
+  return response.data;
+};
+
+export const getCharacters = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
